aa9903921c90494efa698bcf17b63c23a6d6ca1b
git init <butkepavan55@gmail.com>
2021-12-22 02:46:39 -0800

    eform

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+
+function show(){
+    document.querySelector('.textBox').value = type;
+}
+let dropdown = document.querySelector('.dropdown');
+dropdown.onclick = function(){
+    dropdown.classList.toggle('active');
+
+}
+
+
+
+
+
+
+
+
+
